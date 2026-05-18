29aaa3c5873694ae31a96eeadfaedab49fdafbf0
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2026-05-19 04:24:56 +0600

    set up the basic packages for project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+
+const bootstrap = () => {
+  try {
+    app.listen(5000, () => {
+      console.log(`Server is running on port 5000`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+};
+
+bootstrap();
